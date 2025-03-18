1fbb0b5475c3bf2d7cb6b9ed612e395eff728cfc
unknown <--unset.all>
2025-03-18 08:13:59 +0700

    menambahkan generic class dengan method addNewData dan printAllData

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hellp World!");
+﻿class SimpleBaseData<T>() {
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase() {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data){ 
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void printAllData() {
+        for (int i = 0; i < storedData.Count; i++) {
+            Console.WriteLine(storedData[i] + " " + inputDates[i]);
+        }
+    }
+}
+
+class main() { 
+    SimpleBaseData<int> test = new SimpleBaseData<int>();
+    test.AddNewData(10);
+}
